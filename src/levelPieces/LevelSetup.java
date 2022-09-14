22f8a002c8a98fd253cd05999b6ccceeed862609
HenryJohnson26 <108373639+HenryJohnson26@users.noreply.github.com>
2022-09-14 17:32:20 -0600

    added needed instance data and coded accessor methods

---
+++ src/levelPieces/LevelSetup.java
@@ -0,0 +1,44 @@
+package levelPieces;
+
+import java.util.ArrayList;
+
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.Moveable;
+
+public class LevelSetup {
+	
+	private ArrayList<Moveable> movingPieces;
+	private Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+	private ArrayList<GamePiece> gamePieces;
+	private int playerStartLoc = 0;
+	
+	public LevelSetup() {
+		//TODO: initialize instance data as needed
+	}
+	
+	public void createLevel(int levelNum) {
+		//TODO:
+	}
+	
+	public Drawable[] getBoard() {
+		//TODO: return the game board
+		return gameBoard;
+	}
+	
+	public ArrayList<Moveable> getMovingPieces(){
+		//TODO: return arraylist of moving pieces
+		return movingPieces;
+	}
+	
+	public ArrayList<GamePiece> getInteractingPieces(){
+		//TODO: return game pieces
+		return gamePieces;
+	}
+	
+	public int getPlayerStartLoc() {
+		//TODO: return starting location of player
+		return playerStartLoc;
+	}
+
+}
