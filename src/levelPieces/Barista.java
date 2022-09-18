d47d0a5e2028aace122b6b9ac11a35d4509739d6
HenryJohnson26 <108373639+HenryJohnson26@users.noreply.github.com>
2022-09-18 17:06:21 -0600

    updated barista's move and interact methods

--- src/levelPieces/Barista.java
+++ src/levelPieces/Barista.java
@@ -3,6 +3,7 @@ package levelPieces;
 import gameEngine.Drawable;
 import gameEngine.InteractionResult;
 import gameEngine.Moveable;
+import gameEngine.Player;
 
 public class Barista extends GamePiece implements Moveable, Drawable {
 
@@ -20,10 +21,13 @@ public class Barista extends GamePiece implements Moveable, Drawable {
     @Override
     public void move(Drawable[] gameBoard, int playerLocation) {
         //moves one piece at a time torwards the player
+        if(getLocation()<playerLocation)setLocation(getLocation()+1);
+        else setLocation(getLocation()-1);
     }
 
     @Override
     public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+        if(Math.abs(playerLocation-getLocation())==1)return InteractionResult.HIT;
         return null;
     }
 }
